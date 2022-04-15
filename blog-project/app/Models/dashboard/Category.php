ff8480afe7a31da541b736ab5ab063a33dc15156
Ikechukwu-Unegbu <42593643+Ikechukwu-Unegbu@users.noreply.github.com>
2022-04-15 08:33:35 +0100

    m

--- blog-project/app/Models/dashboard/Category.php
+++ blog-project/app/Models/dashboard/Category.php
@@ -2,12 +2,13 @@
 
 namespace App\Models\dashboard;
 
+use Haruncpi\LaravelUserActivity\Traits\Loggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, Loggable;
     public function post(){
         return $this->belongsToMany(Post::class);
     }
--- blog-project/app/Models/dashboard/Post.php
+++ blog-project/app/Models/dashboard/Post.php
@@ -2,12 +2,13 @@
 
 namespace App\Models\dashboard;
 
+use Haruncpi\LaravelUserActivity\Traits\Loggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Loggable;
 
     public function category(){
         return $this->belongsToMany(Category::class);
