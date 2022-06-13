1158f6072beaf8e913536018c153d3d90a163cb3
ikechukwu-unegbu <iunegbu94@yahoo.com>
2022-06-14 00:26:30 +0100

    first laravel package attempt

---
+++ fin-tech-app/package/devdojo/src/DevdojoServiceProvider.php
@@ -0,0 +1,17 @@
+<?php  
+namespace Hp\Devdojo;
+
+use Closure;
+use Illuminate\Support\ServiceProvider;
+
+class DevdojoServiceProvider extends ServiceProvider{
+
+    public function register()
+    {
+        
+    }
+
+    public function boot(){
+        
+    }
+}
